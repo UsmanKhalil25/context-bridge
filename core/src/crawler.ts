b5f80dadd6e6aedb4a72b7c249b11e6b1b5f722f
UsmanKhalil25 <usmankhalil8011@gmail.com>
2026-01-12 22:35:39 +0500

    feat: add a simple crawler

--- core/src/crawler.ts
+++ core/src/crawler.ts
@@ -1,7 +1,23 @@
 export class Crawler {
-  constructor(private readonly baseUrl: string) {}
+  private readonly queue: string[] = [];
+  private readonly visited = new Set<string>();
 
-  run() {
-    console.log({ baseUrl: this.baseUrl });
+  constructor(private readonly baseUrl: string) {
+    this.queue.push(baseUrl);
+  }
+
+  async run() {
+    while (this.queue.length) {
+      const currentUrl = this.queue.shift()!;
+      if (this.visited.has(currentUrl)) continue;
+
+      this.visited.add(currentUrl);
+
+      const response = await fetch(currentUrl);
+      const text = await response.text();
+      console.log({ text });
+      // TODO: extract links and add to queue
+      // make sure that the url is the same origin
+    }
   }
 }
